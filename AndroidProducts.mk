449c1258c804bd47415c91590c29479d77319cc9
DarkJoker360 <simoespo159@gmail.com>
2018-07-13 14:41:55 +0000

    Revert "alice: lineage -> slim"
    This reverts commit 42758706dac9adfc4d8016b75c28a8d480434058.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/slim.mk
+	$(LOCAL_DIR)/lineage.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo slim_alice-userdebug
+add_lunch_combo lineage_alice-userdebug
