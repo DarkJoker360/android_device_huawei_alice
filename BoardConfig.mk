5ed4c63b26f8730b3bc25c64d43639fc75dd42ca
Artem Borisov <dedsa2002@gmail.com>
2020-02-15 15:11:13 +0100

    alice: Enable boot and system server dex-preopt
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: I215ec7ebe95981ef0b953fe938e12b83675795ab
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,16 @@ BOARD_HARDWARE_CLASS := \
     hardware/cyanogen/cmhw \
     $(DEVICE_PATH)/cmhw
 
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
+
 ### FONTS
 EXTENDED_FONT_FOOTPRINT := true
 
