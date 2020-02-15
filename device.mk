c8e10a2f1dde708bdca07d6e1519d44ec3a17dde
Ying Wang <wangying@google.com>
2020-02-15 15:14:53 +0100

    alice: aapt: Set PRODUCT_AAPT_PREBUILT_DPI
    The build system use both PRODUCT_AAPT_PREF_CONFIG and
    PRODUCT_AAPT_PREBUILT_DPI to select prebuilt dpi-specific apks.
    The first match takes precedence.
    
    Alice: Configured for xhdpi by tetsuo55
    
    Change-Id: Iba98af754c3397ddbb27287244db086cb4c43aa6
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -93,6 +93,7 @@ PRODUCT_COPY_FILES += \
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xhdpi 280dpi hdpi tvdpi mdpi ldpi
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
