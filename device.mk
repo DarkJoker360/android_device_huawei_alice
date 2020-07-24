b6309b345e5bba793d21426db950f9b179899736
DarkJoker360 <simoespo159@gmail.com>
2020-07-24 10:28:32 +0200

    alice: Properly include vendor blobs

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-$(call inherit-product-if-exists, vendor/huawei/alice/alice-vendor.mk)
+$(call inherit-product, vendor/huawei/alice/alice-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
