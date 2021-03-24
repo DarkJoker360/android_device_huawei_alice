04971939db0ad368d3e42d3c083a3615eb1c7cc1
Thespartann <danilastefan69@yahoo.ro>
2021-03-24 10:18:09 +0000

    alice: Don't dexpreopt pic
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,6 @@ ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_PIC := true
     endif
   endif
 endif
