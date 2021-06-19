77d60b1f1135031b35bd8d397bf3123d5025f4ed
Muhammad <77672824+muhammad23012009@users.noreply.github.com>
2021-06-19 19:37:24 +0500

    Remove keymaster

--- device.mk
+++ device.mk
@@ -152,9 +152,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.media.treble_omx=false
 
 # Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+# PRODUCT_PACKAGES += \
+#    android.hardware.keymaster@3.0-impl \
+#    android.hardware.keymaster@3.0-service
 
 # USB
 PRODUCT_PACKAGES += \
